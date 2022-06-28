d953828b0e84ac96f75c0fc650069c794d059208
Lesia-Sh <107194070+Lesia-Sh@users.noreply.github.com>
2022-06-28 05:38:15 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Challenge
+// 1. Create a function isWindy and alert 'It is windy'
+function isWindy() {
+  let wind = "Is it windy today?";
+  if ((wind = "Yes")) {
+    alert("It is a windy day");
+  } else {
+    alert("Oh, sorry");
+  }
+}
+isWindy();
+isWindy();
+// 2. Call the function twice
